a4c93516a092d2003a43cdb0a758fc2e501b0733
medsedan <lancehart27909@proton.me>
2026-03-06 08:32:15 +0300

    Update MapFrame.jsx

--- src/components/MapFrame.jsx
+++ src/components/MapFrame.jsx
@@ -4,7 +4,7 @@ export default function MapFrame({ lat, lng }) {
   return (
     <Paper elevation={8} sx={{ p: 2, mb: 3, borderRadius: 3 }}>
       <iframe
-        src={`https://maps.google.com/maps?q=36.5,-1.001&z=15&output=embed`}
+        src="https://maps.google.com/maps?q=-1.107778,36.642778&z=15&output=embed"
         width="100%"
         height="400"
         style={{ borderRadius: "10px" }}
